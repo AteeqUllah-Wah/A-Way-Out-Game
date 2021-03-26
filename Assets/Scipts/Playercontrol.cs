c7ef09dab3182873fcef81de16502dbbb947fa41
Syed-Ahmad-Raza <80562169+Syed-Ahmad-Raza@users.noreply.github.com>
2021-03-26 12:30:58 +0500

    Way out ammendment
    we have added collider and trigger in that game

---
+++ Assets/Scipts/Playercontrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrol : MonoBehaviour
+{
+    [SerializeField] float movespeed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Move_right"))
+        {
+            gameObject.transform.Translate(-Vector3.right * movespeed * Time.deltaTime);
+
+        }
+       else if (Input.GetButton("Move_left"))
+        {
+            gameObject.transform.Translate(Vector3.right * movespeed * Time.deltaTime);
+
+        }
+    }
+}
---
+++ Assets/Scipts/trackmover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackmover : MonoBehaviour
+{
+    [SerializeField] float movetrack = 0f;
+    [SerializeField] GameObject trackprefeb;
+    private float distanceofdeletion =  100f;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(  - Vector3.left * Time.deltaTime * movetrack);
+       // if(gameObject.transform.position.x <=  distanceofdeletion)
+        //{
+       //    Destroy (gameObject.transform.GetChild(0).gameObject);
+         //   distanceofdeletion = distanceofdeletion + 100;
+         //  GameObject temp = Instantiate(trackprefeb, Vector3.zero, Quaternion.identity);
+           // temp.transform.position = new Vector3(gameObject.transform.GetChild(gameObject.transform.childCount - 1).transform.position.x + 100f,0,0);
+          // temp.transform.parent = gameObject.transform;
+       // }
+    }
+}
