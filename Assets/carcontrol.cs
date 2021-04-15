6a39a0903ee9144ecfb31b10d7d0acc2d0205927
Syed-Ahmad-Raza <80562169+Syed-Ahmad-Raza@users.noreply.github.com>
2021-04-15 12:49:13 +0500

    lab assign 1
    here is the background based project

---
+++ Assets/carcontrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carcontrol : MonoBehaviour
+{
+    WheelJoint2D wj;
+    JointMotor2D mo;
+    void Start()
+    {
+        wj = gameObject.GetComponents<WheelJoint2D>()[0];
+        mo = new JointMotor2D();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mo.motorSpeed = -300;
+            mo.maxMotorTorque = 1000;
+            wj.useMotor = true;
+            wj.motor = mo;
+            
+           
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            wj.useMotor = false;
+        }
+    }
+}
---
+++ Assets/findthecamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findthecamera : MonoBehaviour
+{
+    Transform car;
+    void Start()
+    {
+        car = GameObject.Find("car").transform;
+    }
+
+
+    void Update()
+    {
+        if (transform.position.x<car.position.x+1)
+        {
+            transform.Translate(.1f, 0, 0);
+
+        }
+    }
+}
